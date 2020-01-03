f2088027a0f4aed4d9d1973a0b8b40e4b2222763
Yun Jay Kim <yunjaysus@gmail.com>
2020-01-03 19:59:39 +0900

    1912
    1912

--- 김윤재/Chapter3/maxSubarraySum.cpp
+++ 김윤재/Chapter3/maxSubarraySum.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 int main(){
-    
+    int n,o;
+    int maxSum =0;
+    vector<int> arr;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>o;
+        arr.push_back(o);
+    }
+    for (int i =0;i<n;i++){
+        int curSum=0;
+        for(int j=i;j<n;j++){
+            curSum+=arr[j];
+            maxSum=max(maxSum,curSum);
+        }
+    }
+    cout<<maxSum;
 }
