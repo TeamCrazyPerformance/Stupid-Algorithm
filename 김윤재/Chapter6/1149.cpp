66980c335097f5122a219679c0cb5d92df5bf5bf
Yun Jay Kim <yunjaysus@gmail.com>
2020-01-23 00:11:04 +0900

    122

---
+++ 김윤재/Chapter6/1149.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    int prices[1000][3];
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        for (int j=0; j<3; j++){
+            cin>>
+        }
+    }
+    cout<<;
+    return 0;
+}
+
--- 김윤재/Chapter6/2579.cpp
+++ 김윤재/Chapter6/2579.cpp
@@ -6,8 +6,14 @@ int main() {
     vector<int> stairs;
     int score[301]={0};
     cin>>n; //n=계단수
-    for (int i = 0; i < n; i++){cin>>input;stairs.push_back(input);}
-
+    for (int i = 0; i<n; i++){cin>>input;stairs.push_back(input);}
+    //stairs 인덱스가 하나씩 작음
+    score[1]=stairs[0];
+    score[2]=stairs[0]+stairs[1];
+    for(int i = 3; i<=n; i++){
+        score[i]=max(score[i-2]+stairs[i-1],score[i-3]+stairs[i-1]+stairs[i-2]);
+    }
+    
     cout<<score[n];
     return 0;
 }
